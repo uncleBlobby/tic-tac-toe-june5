2d553eedc7242f50d9f25819f2615b83c3d6da82
Dustin <dc.christianson@gmail.com>
2021-06-06 15:13:38 -0600

    basic randomized AI

--- players.js
+++ players.js
@@ -1,8 +1,8 @@
-const player = (id, marker, isActive) => {
+const player = (id, marker, isActive, isAI) => {
     const sayHello = () => console.log(`hello, i'm player ${marker}`);
-    return { id, marker, isActive};
+    return { id, marker, isActive, isAI };
 };
 
-const playerX = player('playerX', 'X', true);
-const playerO = player('playerO', 'O', false);
+const playerX = player('playerX', 'X', true, false);
+const playerO = player('playerO', 'O', false, true);
 
